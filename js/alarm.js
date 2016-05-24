c25abcaf50d151c57043bbf8e17ead2b7ec63fbb
Garrett Biernat and Liam Ellis <garrett+liam@>
2016-05-24 14:03:53 -0700

    added howler and snooze with cancel button functionality

--- js/alarm.js
+++ js/alarm.js
@@ -1,8 +1,13 @@
 exports.alarmClock = function (alarmtime) {
     var time = moment().format('MMMM Do YYYY, h:mm:ss a');
-    var checktime = moment().format('lll');
+    var checktime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
     if(checktime == alarmtime){
       return true;
     }
+}
+
+
+exports.snooze = function(){
+  return moment().add(1, 'm').format('MMMM Do YYYY, h:mm:ss a'); 
 }
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,23 +1,52 @@
 var alarmClock = require('./../js/alarm.js').alarmClock;
+var snooze = require('./../js/alarm.js').snooze;
+var Howl = require('howler').Howl;
 
 $(document).ready(function(){
+  var aud = new Howl({
+  urls: ["../images/rooster-4.wav"],
+  loop: true
+  });
   displayTime();
   $('#datetimepicker1').datetimepicker({
-    format: 'lll'
+    format: 'MMMM Do YYYY, h:mm:ss a'
   });
   $("#alarm").submit(function(e){
     e.preventDefault();
     var alarmtime = $('#datetime').val();
     setInterval(function(){ if(alarmClock(alarmtime)){
+        $("#group").fadeIn();
+        $("#alarm").hide();
         $("#wakeup").text("WAKE UP!!!!!!");
         $('#pic').slideDown(500).effect("shake");
-        document.getElementById('rooster').play();
+        aud.play();
+      } }, 1000);
+  });
+
+  $("#snooze").click(function(e){
+    e.preventDefault();
+    aud.stop();
+    $("#group").hide();
+    var snoozetime = snooze();
+    $("#wakeup").text("");
+    $('#pic').hide();
+    setInterval(function(){ if(alarmClock(snoozetime)){
+        $("#group").fadeIn();
+        $("#wakeup").text("WAKE UP!!!!!!");
+        $('#pic').slideDown(500).effect("shake");
+        aud.play();
       } }, 1000);
   });
 });
 
+$('#cancel').click(function(){
+  aud.stop();
+  $('#alarm').reset().slideDown(1000);
+});
+
+
 function displayTime() {
-    var time = moment().format('HH:mm:ss');
+    var time = moment().format('dddd MMMM Do, YYYY h:mm:ss a');
     $('#time').html(time);
     setTimeout(displayTime, 1000);
 }
